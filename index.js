819dc5a3620679b0a796c76149e65c7e9eefe2a3
luaroxy <luannamsiqueira@gmail.com>
2022-10-09 16:41:57 -0600

    Added append and prepend

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ import LinkedList from "./linkedList.js";
 import Node from "./node.js";
 
 const linkedList = new LinkedList();
+const newNode = new Node("test1");
+const head = linkedList.prepend(newNode);
+
+const newNode2 = new Node("test2");
+const nodee = linkedList.append(newNode2);
+
+const newNode3 = new Node("test3");
+const nodee2 = linkedList.append(newNode3);
 
 console.log(linkedList);
--- linkedList.js
+++ linkedList.js
@@ -2,7 +2,19 @@ export default class LinkedList
 {
     constructor()
     {
-        this.list = {};
+        this.head = {};
     }
     
+    prepend(node){
+        this.head = node;
+    }
+
+    append(node){
+        if(this.head == null) this.prepend(node); 
+        else {
+            let tmp = this.head;
+            while(tmp.nextNode != null) tmp = tmp.nextNode;
+            tmp.nextNode = node;
+        }
+    }
 }
