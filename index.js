c711ee76e197f8457cb48926341331c28ade54ab
luaroxy <luannamsiqueira@gmail.com>
2022-10-10 11:39:25 -0600

    refactored

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 import LinkedList from "./linkedList.js";
 
+// Tests
 const linkedList = new LinkedList();
 
 linkedList.prepend("test1");
-linkedList.append("test2");;
+linkedList.append("test2");
 linkedList.append("test3");
-
-console.log(linkedList);
-console.log(linkedList.size());
-console.log(linkedList.head());
-console.log(linkedList.tail());
-console.log(linkedList.at(3));
+console.log(linkedList.toString()); // (test1) -> (test2) -> (test3) -> null
+console.log(linkedList.size()); // 3
+console.log(linkedList.head()); // return head Node
+console.log(linkedList.tail()); // Node { value: 'test3', nextNode: null }
+console.log(linkedList.at(2)); // Node { value: 'test3', nextNode: null }
+console.log(linkedList.at(4)); // There is no item for this index
 linkedList.pop();
-console.log(linkedList);
-console.log(linkedList.contains("test1"));
-console.log(linkedList.find("test2"));
-console.log(linkedList.toString());
+console.log(linkedList.toString()); // (test1) -> (test2) -> null
+console.log(linkedList.contains("test1")); // true
+console.log(linkedList.find("test2")); // 1
 linkedList.prepend("test3");
-console.log(linkedList.toString());
+console.log(linkedList.toString()); // (test3) -> (test1) -> (test2) -> null
 linkedList.insertAt("test4",2);
-console.log(linkedList.toString());
+console.log(linkedList.toString()); // (test3) -> (test1) -> (test4) -> (test2) -> null
 linkedList.insertAt("test5",8);
-console.log(linkedList.toString());
+console.log(linkedList.toString()); // (test3) -> (test1) -> (test4) -> (test2) -> (test5) -> null
 linkedList.removeAt(2);
-console.log(linkedList.toString());
+console.log(linkedList.toString()); // (test3) -> (test1) -> (test2) -> (test5) -> null
--- linkedList.js
+++ linkedList.js
@@ -1,124 +1,121 @@
 import Node from "./node.js";
 
-export default class LinkedList
-{
-    constructor()
-    {
-        this.listHead = null;
-    }
-    
-    prepend(value){
-        let tmp = null;
-        if (this.listHead != null) tmp = this.listHead; 
-        this.listHead = new Node(value);
-        this.listHead.nextNode = tmp;
-    }
+export default class LinkedList {
+  constructor() {
+    this.listHead = null;
+  }
 
-    append(value){
-        if(this.listHead == null) this.prepend(value); 
-        else {
-            let tmp = this.listHead;
-            while(tmp.nextNode != null) tmp = tmp.nextNode;
-            tmp.nextNode = new Node(value);
-        }
-    }
+  prepend(value) {
+    let tmp = null;
+    if (this.listHead != null) tmp = this.listHead;
+    this.listHead = new Node(value);
+    this.listHead.nextNode = tmp;
+  }
 
-    size(){
-        let tmp = this.listHead;
-        let counter = 0;
-        while(tmp != null){
-            counter++;
-            tmp = tmp.nextNode;
-        }
-        return counter;
+  append(value) {
+    if (this.listHead == null) this.prepend(value);
+    else {
+      let tmp = this.listHead;
+      while (tmp.nextNode != null) tmp = tmp.nextNode;
+      tmp.nextNode = new Node(value);
     }
+  }
 
-    head(){
-        return this.listHead;
+  size() {
+    let tmp = this.listHead;
+    let counter = 0;
+    while (tmp != null) {
+      counter++;
+      tmp = tmp.nextNode;
     }
+    return counter;
+  }
 
-    tail(){
-        let tmp = this.listHead;
-        while(tmp.nextNode != null) tmp = tmp.nextNode;
-        return tmp;
-    }
+  head() {
+    return this.listHead;
+  }
 
-    at(index){
-        let tmp = this.listHead;
-        for(let i=0; i < index; i++){ 
-            tmp = tmp.nextNode;
-            if (tmp == null) return "There is no item for this index";
-        }
-        return tmp;
-    }
+  tail() {
+    let tmp = this.listHead;
+    while (tmp.nextNode != null) tmp = tmp.nextNode;
+    return tmp;
+  }
 
-    pop(){
-        let cur = this.listHead;
-        let prev = null;
-        while(cur.nextNode != null){
-            prev = cur;
-            cur = cur.nextNode;
-        }
-        prev.nextNode = null;
+  at(index) {
+    let tmp = this.listHead;
+    for (let i = 0; i < index; i++) {
+      tmp = tmp.nextNode;
+      if (tmp == null) return "There is no item for this index";
     }
-    
-    contains(value){
-        let tmp = this.listHead;
-        while(tmp != null){
-            if (tmp.value == value) return true;
-            tmp = tmp.nextNode;
-        }
-        return false;
-    }
+    return tmp;
+  }
 
-    find(value){
-        let tmp = this.listHead;
-        let index = 0;
-        while(tmp != null){
-            if (tmp.value == value) return index;
-            tmp = tmp.nextNode;
-            index++;
-        }
-        return null;
+  pop() {
+    let cur = this.listHead;
+    let prev = null;
+    while (cur.nextNode != null) {
+      prev = cur;
+      cur = cur.nextNode;
     }
+    prev.nextNode = null;
+  }
 
-    toString(){
-        let tmp = this.listHead;
-        let stringList = "";
-        while(tmp != null){ 
-            stringList += `(${tmp.value}) -> `;
-            tmp = tmp.nextNode;
-        }
-        return stringList += 'null';
+  contains(value) {
+    let tmp = this.listHead;
+    while (tmp != null) {
+      if (tmp.value === value) return true;
+      tmp = tmp.nextNode;
     }
+    return false;
+  }
 
-    insertAt(value, index){
-        if(this.listHead == null) this.prepend(value); 
-        else {
-            let cur = this.listHead;
-            let prev = null;
-            for(let i=0; i < index; i++){ 
-                prev = cur;
-                cur = cur.nextNode;
-                if (cur == null) break; //if index is bigger than end of list, add node at end of list
-            }
-            let tmp = new Node(value);
-            prev.nextNode = tmp;
-            tmp.nextNode = cur;
-        }
+  find(value) {
+    let tmp = this.listHead;
+    let index = 0;
+    while (tmp != null) {
+      if (tmp.value === value) return index;
+      tmp = tmp.nextNode;
+      index++;
     }
+    return null;
+  }
 
-    removeAt(index){
-        if(this.listHead == null) return "List is already empty";
-        else {
-            let cur = this.listHead;
-            let prev = null;
-            for(let i=0; i < index; i++){ 
-                prev = cur;
-                cur = cur.nextNode;
-                if (cur == null) return "There is no item for this index";
-            }
-            prev.nextNode = cur.nextNode;
-        }
+  toString() {
+    let tmp = this.listHead;
+    let stringList = "";
+    while (tmp != null) {
+      stringList += `(${tmp.value}) -> `;
+      tmp = tmp.nextNode;
     }
+    return (stringList += "null");
+  }
+
+  insertAt(value, index) {
+    if (this.listHead == null) this.prepend(value);
+    else {
+      let cur = this.listHead;
+      let prev = null;
+      for (let i = 0; i < index; i++) {
+        prev = cur;
+        cur = cur.nextNode;
+        if (cur == null) break; // if index is bigger than end of list, add node at end of list
+      }
+      const tmp = new Node(value);
+      prev.nextNode = tmp;
+      tmp.nextNode = cur;
+    }
+  }
+
+  removeAt(index) {
+    if (this.listHead == null) return "List is already empty";
+
+    let cur = this.listHead;
+    let prev = null;
+    for (let i = 0; i < index; i++) {
+      prev = cur;
+      cur = cur.nextNode;
+      if (cur == null) return "There is no item for this index";
+    }
+    prev.nextNode = cur.nextNode;
+  }
 }
